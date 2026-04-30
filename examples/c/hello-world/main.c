2c45325259bb1e733f377f76f750fe5f37f1799e
warpanit <warpaint9299@email.com>
2026-04-30 13:14:11 +0800

    feat(example): add the hello world template of c

---
+++ examples/c/hello-world/main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+int main( int argc, char** argv)
+{
+  
+  printf("Hello world!\n");
+  return EXIT_SUCCESS;
+}
